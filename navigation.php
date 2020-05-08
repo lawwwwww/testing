c65743bd921825a700dae790748897eb785c333d
lawwwwww <48543658+lawwwwww@users.noreply.github.com>
2020-05-08 21:47:03 +0800

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Order Details</title>
+<link rel="stylesheet" href="navigation.css?am={random number/string}">
+
+</head>
+<header>
+<div class="navvv">
+	<p id="imageee"><img  src='navi.png' alt='navigation_icon' height='50px' width='50px'></p>
+	<ul id="navv">
+	  <li class="vava"><a href="uploadfile.php">Manage Trip</a></li>
+	  <li class="vava"><a href="#">Manage Container</a></li>
+	  <li class="vava"><a href="#">Manage Driver</a></li>
+	  <li class="vava"><a href="#">Manage Holiday</a></li>
+	  <li class="vava"><a href="#">Manage Truck</a></li>
+	  <li class="tata"><a href="#">Log Out?</a></li>
+	  
+	  
+	</ul>
+	</div>
+</header>
+<body>
+
+</body>
+</html>
---
+++ trip.php
@@ -0,0 +1,121 @@
+<?php
+	$conn=mysqli_connect("localhost","root","","trienekendb");
+	
+	if($conn->connect_error)
+	{
+		die("Connection failed:".$conn->connect_error);
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+	<title>Trieneken</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="Cafe">
+</head>
+<header>
+	<?php include'navigation.php'?>
+</header>
+<a style="margin-right:10px;" href="uploadfile.php">Upload DHLS</a>
+<a href="trip.php">View Trip</a>
+
+
+	<style>
+li{list-style-type:none;}
+	
+	body{
+		margin-left:100px;
+		margin-top:20px;
+	}
+	
+	table {
+  border-collapse: collapse;
+  max-width:1700px; margin-right:10px;
+}
+
+table, th, td {
+  border: 1px solid black;
+}
+
+h2{
+	margin-top:30px;
+}
+
+a { color: #000000; }
+a:visited { #FF0000; }
+a:hover,a:active,a:focus{#0F4CC8;}
+	</style>
+	
+	<h2>Trip</h2>
+	<table>
+	<tr>
+		<td>Customer name</td>
+		<td>Area ID</td>
+		<td>Container retrieve</td>
+		<td>Container placed</td>
+		<td>Collection time</td>
+		<td>Collection type</td>
+		<td>Waste type</td>
+		<td>Trip ID</td>
+		<td>Customer signature</td>
+		<td>Ship date</td>
+		<td>Return date</td>
+		<td>Driver ID</td>
+		<td>Admin ID</td>
+		<td>Assign status</td>
+		<td>Trip status</td>
+		<td>Image</td>
+		<td>Instruction</td>
+		<td>Truck ID</td>
+		<td>Sales order no.</td>
+		<td>Address</td>
+		<td>Container size</td>
+	</tr>	
+	<?php
+		$sql="SELECT * FROM triptable";
+		$result=mysqli_query($conn,$sql);
+		
+		if(mysqli_num_rows($result)>0)
+		{
+			while($row=mysqli_fetch_array($result))
+			{
+		?>
+		<tr>
+		<td><?php echo $row["cust_name"]?></td>
+		<td><?php echo $row["areaID"]?></td>
+		<td><?php echo $row["container_retrieve"]?></td>
+		<td><?php echo $row["container_placed"]?></td>
+		<td><?php echo $row["collection_time"]?></td>
+		<td><?php echo $row["collection_type"]?></td>
+		<td><?php echo $row["waste_type"]?></td>
+		<td><?php echo $row["tripID"]?></td>
+		<td><?php echo $row["customer_sign"]?></td>
+		<td><?php echo $row["ship_date"]?></td>
+		<td><?php echo $row["return_date"]?></td>
+		<td><?php echo $row["driverID"]?></td>
+		<td><?php echo $row["adminID"]?></td>
+		<td><?php echo $row["assign_status"]?></td>
+		<td><?php echo $row["trip_status"]?></td>
+		<td><?php echo $row["image"]?></td>
+		<td><?php echo $row["instruction"]?></td>
+		<td><?php echo $row["truckID"]?></td>
+		<td><?php echo $row["salesOrderNo"]?></td>
+		<td><?php echo $row["address"]?></td>
+		<td><?php echo $row["container_size"]?></td>
+		</tr>
+	<?php		
+			}
+		}
+	?>	
+		</table>
+
+	
+</body>
+</html>
+
+
+<?php
+$conn->close();
+?>
